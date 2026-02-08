e12ffa602014b7e885ad36dc7776aeddc91900f3
Abdul Ghaffar <agscontact777@gmail.com>
2026-02-08 14:48:11 +0500

    Created the connection with postgres

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
 
-func main(){
-	
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), "postgres://learner:3069921@localhost:5432/testdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(context.Background())
+	fmt.Println("Connected to Postgres")
 }
